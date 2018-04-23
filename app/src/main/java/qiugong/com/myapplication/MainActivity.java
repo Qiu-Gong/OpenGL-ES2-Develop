9f35d6777119446873c692d59dbc36939fd7830c
邱志雄 <qzx@meitu.com>
2018-04-23 22:33:19 +0800

    [需求]提交工程

---
+++ app/src/main/java/qiugong/com/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package qiugong.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+}
