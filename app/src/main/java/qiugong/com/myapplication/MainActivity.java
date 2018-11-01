b692af4f144f8547df52579a245db04dfda4952f
邱志雄 <qzx@meitu.com>
2018-11-02 00:30:40 +0800

    【学习】准备开始

--- app/src/main/java/qiugong/com/myapplication/MainActivity.java
+++ app/src/main/java/qiugong/com/myapplication/MainActivity.java
@@ -1,24 +1,65 @@
 package qiugong.com.myapplication;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        glSurfaceView = new GLSurfaceView(this);
 
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager == null) {
+            return;
+        }
 
-            }
-        });
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        final boolean supportsEs2 =
+                configurationInfo.reqGlEsVersion >= 0x20000
+                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                        && (Build.FINGERPRINT.startsWith("generic")
+                        || Build.FINGERPRINT.startsWith("unknown")
+                        || Build.MODEL.contains("google_sdk")
+                        || Build.MODEL.contains("Emulator")
+                        || Build.MODEL.contains("Android SDK built for x86")));
+
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setRenderer(new FirstOpenGLProjectRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        setContentView(glSurfaceView);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (rendererSet) {
+            glSurfaceView.onResume();
+        }
+    }
 }
