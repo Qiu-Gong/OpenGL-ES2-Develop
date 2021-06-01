dc70a67870ea70e3fd7f3b285ed6c9d29d89c5dc
qiuzhixiong <qiuzhixiong@yidian-inc.com>
2021-06-01 23:34:12 +0800

    [CHG] Chapter 9

--- opengl/src/main/cpp/common/esShader.c
+++ opengl/src/main/cpp/common/esShader.c
@@ -18,6 +18,10 @@
 #include "esUtil.h"
 #include <stdlib.h>
 
+// Defines
+//
+#define ES_PI  (3.14159265f)
+
 //////////////////////////////////////////////////////////////////
 //
 //  Private Functions
@@ -159,4 +163,99 @@ GLuint ESUTIL_API esLoadProgram ( const char *vertShaderSrc, const char *fragSha
    glDeleteShader ( fragmentShader );
 
    return programObject;
+}
+
+/// \brief Generates geometry for a sphere.  Allocates memory for the vertex data and stores
+///        the results in the arrays.  Generate index list for a TRIANGLE_STRIP
+/// \param numSlices The number of slices in the sphere
+/// \param vertices If not NULL, will contain array of float3 positions
+/// \param normals If not NULL, will contain array of float3 normals
+/// \param texCoords If not NULL, will contain array of float2 texCoords
+/// \param indices If not NULL, will contain the array of indices for the triangle strip
+/// \return The number of indices required for rendering the buffers (the number of indices stored in the indices array
+///         if it is not NULL ) as a GL_TRIANGLE_STRIP
+//
+int ESUTIL_API esGenSphere ( int numSlices, float radius, GLfloat **vertices, GLfloat **normals,
+                             GLfloat **texCoords, GLuint **indices )
+{
+   int i;
+   int j;
+   int numParallels = numSlices / 2;
+   int numVertices = ( numParallels + 1 ) * ( numSlices + 1 );
+   int numIndices = numParallels * numSlices * 6;
+   float angleStep = ( 2.0f * ES_PI ) / ( ( float ) numSlices );
+
+   // Allocate memory for buffers
+   if ( vertices != NULL )
+   {
+      *vertices = malloc ( sizeof ( GLfloat ) * 3 * numVertices );
+   }
+
+   if ( normals != NULL )
+   {
+      *normals = malloc ( sizeof ( GLfloat ) * 3 * numVertices );
+   }
+
+   if ( texCoords != NULL )
+   {
+      *texCoords = malloc ( sizeof ( GLfloat ) * 2 * numVertices );
+   }
+
+   if ( indices != NULL )
+   {
+      *indices = malloc ( sizeof ( GLuint ) * numIndices );
+   }
+
+   for ( i = 0; i < numParallels + 1; i++ )
+   {
+      for ( j = 0; j < numSlices + 1; j++ )
+      {
+         int vertex = ( i * ( numSlices + 1 ) + j ) * 3;
+
+         if ( vertices )
+         {
+            ( *vertices ) [vertex + 0] = radius * sinf ( angleStep * ( float ) i ) *
+                                         sinf ( angleStep * ( float ) j );
+            ( *vertices ) [vertex + 1] = radius * cosf ( angleStep * ( float ) i );
+            ( *vertices ) [vertex + 2] = radius * sinf ( angleStep * ( float ) i ) *
+                                         cosf ( angleStep * ( float ) j );
+         }
+
+         if ( normals )
+         {
+            ( *normals ) [vertex + 0] = ( *vertices ) [vertex + 0] / radius;
+            ( *normals ) [vertex + 1] = ( *vertices ) [vertex + 1] / radius;
+            ( *normals ) [vertex + 2] = ( *vertices ) [vertex + 2] / radius;
+         }
+
+         if ( texCoords )
+         {
+            int texIndex = ( i * ( numSlices + 1 ) + j ) * 2;
+            ( *texCoords ) [texIndex + 0] = ( float ) j / ( float ) numSlices;
+            ( *texCoords ) [texIndex + 1] = ( 1.0f - ( float ) i ) / ( float ) ( numParallels - 1 );
+         }
+      }
+   }
+
+   // Generate the indices
+   if ( indices != NULL )
+   {
+      GLuint *indexBuf = ( *indices );
+
+      for ( i = 0; i < numParallels ; i++ )
+      {
+         for ( j = 0; j < numSlices; j++ )
+         {
+            *indexBuf++  = i * ( numSlices + 1 ) + j;
+            *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + j;
+            *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + ( j + 1 );
+
+            *indexBuf++ = i * ( numSlices + 1 ) + j;
+            *indexBuf++ = ( i + 1 ) * ( numSlices + 1 ) + ( j + 1 );
+            *indexBuf++ = i * ( numSlices + 1 ) + ( j + 1 );
+         }
+      }
+   }
+
+   return numIndices;
 }
